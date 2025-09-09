813abbad66d6aa9de0761ed9948ef0c78aacf991
AceKokuren <acekokuren@gmail.com>
2025-09-09 18:09:11 +0100

    Configured App to load single window only, and present a title and basic HTML elements

--- esmodules/script.js
+++ esmodules/script.js
@@ -4,18 +4,49 @@ Hooks.on("renderActorDirectory", function (app, html) {
     const manageUserButton =
         `<button class="manage-users"><i class="fa-solid fa-users"></i> Manage Users<i class="fa-solid fa-users"></i></button>`;
 
-    html.insertAdjacentHTML('beforeend', manageUserButton);
+    html.querySelector('footer').insertAdjacentHTML('beforeend', manageUserButton);
     html.querySelector(".manage-users").addEventListener("click", () => (new UserManagement()).render({force: true}));
 })
 
 const { ApplicationV2, HandlebarsApplicationMixin } = foundry.applications.api;
 
 class UserManagement extends HandlebarsApplicationMixin(ApplicationV2) {
+    static DEFAULT_OPTIONS = {
+        id: 'user-management-tool',
+        classes: [],
+        tag: "form",
+        window: {
+            frame: true,
+            positioned: true,
+            title: "User Management Tool",
+            icon: "",
+            controls: [],
+            minimizable: true,
+            resizable: false,
+            contentTag: "section",
+            contentClasses: []
+        },
+        actions: {},
+        form: {
+            handler: undefined,
+            submitOnChange: false,
+            closeOnSubmit: false
+        },
+        position: {
+            width: "auto",
+            height: "auto"
+        }
+    };
 
-    _replaceHTML(result, content, options) {
-        const text = `<p>Hello World</p>`;
-        content = text;
-        super._replaceHTML(result, content, options);
-        
+    static PARTS = {
+        'management-window': {template: '/modules/user-management-tool/templates/user-management-window.hbs'},
     }
+
+
+
+    // _replaceHTML(result, content, options) {
+    //     const text = `<p>Hello World</p>`;
+    //     content = text;
+    //     super._replaceHTML(result, content, options);
+    // }
 }
